f9d268b0657601b1dbd59bbac1e8d880b3244100
just_xolotl <157688992+xolotlwastaken@users.noreply.github.com>
2024-08-09 18:36:34 +0800

    Create verifyEmailWithToken.dart

---
+++ verifyEmailWithToken.dart
@@ -0,0 +1,41 @@
+// Automatic FlutterFlow imports
+import '/backend/supabase/supabase.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart'; // Imports other custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<bool> verifyEmailWithToken(
+  String email,
+  String? token,
+) async {
+  // Add your function code here!
+  // instantiate Supabase client
+  final supabase = Supabase.instance.client;
+
+  try {
+    // call the supabase verifyOTP function
+    // if successful, a response with the user and session is returned
+    final AuthResponse res = await supabase.auth.verifyOTP(
+      type: OtpType.signup,
+      token: token ?? " ",
+      email: email,
+    );
+
+    // return true if session is not null (i.e. user has signed in)
+    return res.session != null;
+  } on AuthException catch (e) {
+    // catch any authenticatino errors and print them to the console
+    print('Error de autenticación: ${e.message}');
+    return false;
+  } catch (error) {
+    // catch any other errors
+    print(error);
+    return false;
+  }
+}
